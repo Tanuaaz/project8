8344558e677e0462d7bbc38d9949dbce7638cb48
Tanuaaz <73118470+Tanuaaz@users.noreply.github.com>
2021-04-19 08:34:18 +0700

    Update project8.py

--- project8.py
+++ project8.py
@@ -1,49 +1,45 @@
+'''This program draws a fractal tree'''
+
 import turtle
 from random import randint
+
+#setting the main paramets
 t = turtle.Turtle()
+t.color('magenta')
 t.screen.bgcolor('black')
+t.left(90)
+tree_level = 13
+branch_length = 120
+angle = randint(0, 45)
 
-t.lt(90)
-
-lv = 13
-l = 120
-s = int (randint(0, 45))
-
-t.width(lv)
+t.width(tree_level)
 
 t.penup()
-t.bk(l)
-
+t.back(branch_length)
 t.pendown()
-t.fd(l)
-color = [0.35, 0.2, 0.0]
+t.forward(branch_length)
 
-def draw_tree(l, level):
+def draw_tree(branch_length, level):
+    '''This function draws the main tree'''
     width = t.width()  # save the current pen width
+    t.width(width * 3 / 4)  # narrow the pen width
+    branch_length = 3 / 4 * branch_length
+    t.left(angle)
+    t.forward(branch_length)
 
-    t.width(width * 3.0 / 4.0)  # narrow the pen width
-    t.color(color)
-    l = 3.0 / 4.0 * l
+    if level < tree_level:
+        draw_tree(branch_length, level + 1)
+    t.back(branch_length)
+    t.right(2 * angle)
+    t.forward(branch_length)
 
-    t.lt(s)
-    t.fd(l)
-
-    if level < lv:
-        draw_tree(l, level + 1)
-    t.bk(l)
-    t.rt(2 * s)
-    t.fd(l)
-    color[1] += 0.04
-
-    if level < lv:
-        draw_tree(l, level + 1)
-    t.bk(l)
-    t.lt(s)
+    if level < tree_level:
+        draw_tree(branch_length, level + 1)
+    t.back(branch_length)
+    t.left(angle)
 
     t.width(width)  # restore the previous pen width
 
 t.speed("fastest")
-
-draw_tree(l, 2)
-
+draw_tree(branch_length, 2)
 turtle.done()
